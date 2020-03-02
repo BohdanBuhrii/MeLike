d03d8a6901c37d2a1981aa0a1448ac3cd82ed496
BohdanBuhrii <bobuhrii@gmail.com>
2020-03-02 12:32:00 +0200

    Add basic pages

--- MeLike.App/Startup.cs
+++ MeLike.App/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MeLike.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MeLike.App.Data;
 
 namespace MeLike.App
 {
@@ -28,7 +22,7 @@ namespace MeLike.App
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.ConfigureServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
